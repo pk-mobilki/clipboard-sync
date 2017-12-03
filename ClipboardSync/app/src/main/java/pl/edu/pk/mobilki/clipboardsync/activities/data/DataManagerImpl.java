f6483d1ffa931ea8583b7fd7840138d3ef42b4bf
mati792 <mati792@onet.pl>
2017-12-04 00:56:53 +0100

    add dataBase

---
+++ ClipboardSync/app/src/main/java/pl/edu/pk/mobilki/clipboardsync/activities/data/DataManagerImpl.java
@@ -0,0 +1,25 @@
+package pl.edu.pk.mobilki.clipboardsync.activities.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by mati on 2017-12-04.
+ */
+
+public class DataManagerImpl {
+    private Context context;
+    private SQLiteDatabase db;
+
+
+    private String DATABASE_NAME = "db";
+
+    public DataManagerImpl(Context context) {
+        this.context = context;
+
+        SQLiteOpenHelper openHelper = new OpenHelper(this.context, DATABASE_NAME, null, 1);
+        db = openHelper.getWritableDatabase();
+    }
+}
+
---
+++ ClipboardSync/app/src/main/java/pl/edu/pk/mobilki/clipboardsync/activities/model/Clipboard.java
@@ -0,0 +1,30 @@
+package pl.edu.pk.mobilki.clipboardsync.activities.model;
+
+
+/**
+ * Created by mati on 2017-12-04.
+ */
+
+public class Clipboard {
+    private long id;
+    private String text;
+    private String data_time;
+
+    public Clipboard(long id, String text, String data_time){
+        this.id = id;
+        this.text = text;
+        this.data_time = data_time;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public String getData_time(){
+        return data_time;
+    }
+}
